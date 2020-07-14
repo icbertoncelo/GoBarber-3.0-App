be26935666d49688ae73443b59ce1486ba8b0c12
ian.bertoncelo <ian.bertoncelo@infracommerce.com.br>
2020-07-14 09:37:20 -0300

    Add provider horizontal list on create appointment screen

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,6 +1,79 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-const CreateAppointment: React.FC = () => <View />;
+import { useAuth } from '../../contexts/auth';
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
+} from './styles';
+
+import api from '../../services/api';
+import { IProvider } from './types';
+
+interface RouteParams {
+  providerId: string;
+}
+
+const CreateAppointment: React.FC = () => {
+  const route = useRoute();
+  const { providerId } = route.params as RouteParams;
+
+  const { goBack } = useNavigation();
+  const { user } = useAuth();
+
+  const [providers, setProviders] = useState<IProvider[]>([]);
+  const [selectedProvider, setSelectedProvider] = useState(providerId);
+
+  useEffect(() => {
+    async function loadProviders(): Promise<void> {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => goBack()}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+
+      <ProvidersListContainer>
+        <ProvidersList
+          data={providers}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider }) => (
+            <ProviderContainer
+              onPress={() => setSelectedProvider(provider.id)}
+              selected={provider.id === selectedProvider}
+            >
+              <ProviderAvatar source={{ uri: provider.avatar_url }} />
+              <ProviderName selected={provider.id === selectedProvider}>
+                {provider.name}
+              </ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
+    </Container>
+  );
+};
 
 export default CreateAppointment;
